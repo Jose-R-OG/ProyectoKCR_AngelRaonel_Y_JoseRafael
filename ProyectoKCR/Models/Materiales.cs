0d3965a6b279b5b2d8a09ed84e513fac5c139df4
Jose <Joser2711m@gmail.com>
2025-11-21 11:36:00 -0400

    Craecion de carpetas models, creacion de modelos correspodientes e instalacion de paquetes.

---
+++ ProyectoKCR/Models/Materiales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoKCR.Models;
+
+public class Materiales
+{
+    [Key]
+    public int IdMaterial { get; set; }
+
+    [Required, MaxLength(255)]
+    public string Nombre { get; set; }
+
+    public int Existencia { get; set; }
+
+    [Required]
+    public decimal PrecioUnitario { get; set; }
+
+    public ICollection<DetallePreFactura> DetallesPreFactura { get; set; } = new List<DetallePreFactura>();
+}
---
+++ ProyectoKCR/Models/Turnos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoKCR.Models;
+
+public class Turnos
+{
+    [Key]
+    public int IdTurno { get; set; }
+    [Required, MaxLength(50)]
+    public string NumTurno { get; set; }
+    public DateTime Fecha { get; set; }
+
+    [ForeignKey("Cliente")]
+    public int IdCliente { get; set; }
+    public Clientes Cliente { get; set; }
+
+    [ForeignKey("Servicio")]
+    public int IdServicio { get; set; }
+    public Servicios Servicio { get; set; }
+}
