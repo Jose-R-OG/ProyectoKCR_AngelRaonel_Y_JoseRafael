5299665b5ce444e91f758152a96885862d1088f9
Jose <Joser2711m@gmail.com>
2025-11-28 12:33:47 -0400

    Creacion de service de factura

---
+++ KCR/Services/PreFacturaService.cs
@@ -0,0 +1,107 @@
+﻿using KCR.Data;
+using KCR.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace KCR.Services;
+
+public class PreFacturaService(IDbContextFactory<ApplicationDbContext> DbFactory)
+{
+    public async Task<bool> Existe(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.preFacturas.AnyAsync(p => p.IdPreFactura == id);
+    }
+
+    public async Task AfectarExistencia(PreFacturaDetalles[] detalle, TipoOperacion tipoOperacion)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        foreach (var item in detalle)
+        {
+            var material = await contexto.materiales.SingleAsync(m => m.IdMaterial == item.Servicios.IdMaterial);
+            if (tipoOperacion == TipoOperacion.Suma)
+                material.Existencia += item.Cantidad;
+            else
+                material.Existencia -= item.Cantidad;
+            await contexto.SaveChangesAsync();
+        }
+    }
+
+    public async Task<bool> Insertar(PreFacturas preFactura)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.preFacturas.Add(preFactura);
+        await AfectarExistencia(preFactura.PreFacturaDetalles.ToArray(), TipoOperacion.Resta);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Modificar(PreFacturas preFactura)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var original = await contexto.preFacturas
+            .Include(p => p.PreFacturaDetalles)
+            .AsNoTracking()
+            .SingleOrDefaultAsync(p => p.IdPreFactura == preFactura.IdPreFactura);
+
+        if (original == null) return false;
+
+        await AfectarExistencia(original.PreFacturaDetalles.ToArray(), TipoOperacion.Suma);
+
+        contexto.preFacturaDetalles.RemoveRange(original.PreFacturaDetalles);
+
+        contexto.Update(preFactura);
+
+        await AfectarExistencia(preFactura.PreFacturaDetalles.ToArray(), TipoOperacion.Resta);
+
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Guardar(PreFacturas preFactura)
+    {
+        if (!await Existe(preFactura.IdPreFactura))
+            return await Insertar(preFactura);
+        else
+            return await Modificar(preFactura);
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var preFactura = await Buscar(id);
+
+        await AfectarExistencia(preFactura.PreFacturaDetalles.ToArray(), TipoOperacion.Resta);
+        contexto.preFacturaDetalles.RemoveRange(preFactura.PreFacturaDetalles);
+        contexto.preFacturas.Remove(preFactura);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<PreFacturas?> Buscar(int Id)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.preFacturas.Include(p => p.PreFacturaDetalles).FirstOrDefaultAsync(p => p.IdPreFactura == Id);
+    }
+
+    public async Task<List<PreFacturas>> Listar(Expression<Func<PreFacturas, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.preFacturas.Include(p => p.PreFacturaDetalles).Where(criterio).AsNoTracking().ToListAsync();
+    }
+
+    public async Task<List<Servicios>> ListarServicios()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.servicios.Include(p => p.IdMaterial).Where(s => s.IdServicio > 0).AsNoTracking().ToListAsync();
+    }
+
+    public async Task<List<Materiales>> ListarMateriales()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.materiales.Where(m => m.IdMaterial > 0).AsNoTracking().ToListAsync();
+    }
+}
+
+public enum TipoOperacion
+{
+    Suma = 1,
+    Resta = 2
+}
