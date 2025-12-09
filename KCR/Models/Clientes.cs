8a923dd84d0dce2d02827dee4297dde1c150801e
angel <angel_guerrero1@ucne.edu.do>
2025-12-08 21:28:43 -0400

    Validaciones

--- KCR/Models/Clientes.cs
+++ KCR/Models/Clientes.cs
@@ -6,9 +6,18 @@ public class Clientes
 {
     [Key]
     public int IdCliente { get; set; }
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s\.-]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string Nombres { get; set; }
-    public string? Cedula { get; set; } 
+
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "La Cédula debe tener 11 dígitos.")]
+    [RegularExpression(@"^\d{11}$", ErrorMessage = "Solo se permiten 11 dígitos numéricos.")]
+    public string? Cedula { get; set; }
+
+    [RegularExpression(@"^\d{10,}$", ErrorMessage = "El teléfono solo debe contener números.")]
     public string? Telefono { get; set; }
+
+    [EmailAddress(ErrorMessage = "Formato de correo inválido.")]
     public string? Correo { get; set; }
     public DateTime Fecha { get; set; }
 
--- KCR/Models/Empleados.cs
+++ KCR/Models/Empleados.cs
@@ -6,13 +6,21 @@ public class Empleados
 {
     [Key]
     public int IdEmpleado { get; set; }
-    [Required]
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s\.-]+$", ErrorMessage = "El nombre solo puede contener letras y espacios.")]
     public string Nombre { get; set; }
-    [Required]
-    public string Usuario { get; set; } 
+
+    [Required(ErrorMessage = "El usuario (Email) es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El formato del usuario debe ser un correo electrónico válido.")]
+    public string Usuario { get; set; }
+
     [Required, MaxLength(255)]
-    public string Clave { get; set; } 
-    public string? Cedula { get; set; } 
+    public string Clave { get; set; }
+
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "La Cédula debe tener 11 dígitos.")]
+    [RegularExpression(@"^\d{11}$", ErrorMessage = "Solo se permiten 11 dígitos numéricos.")]
+    public string? Cedula { get; set; }
+
     public string? Cargo { get; set; }
 
     public ICollection<PreFacturas> PreFacturas { get; set; } = new List<PreFacturas>();
